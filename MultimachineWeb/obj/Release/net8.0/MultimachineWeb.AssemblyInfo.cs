580984698e129c52fe457d43436f3b1b36a1a2af
Mohammed Amin <mohammed.amin@saa.ai>
2026-09-07 14:16:37 +0530

    CICD

--- MultimachineWeb/obj/Release/net8.0/MultimachineWeb.AssemblyInfo.cs
+++ MultimachineWeb/obj/Release/net8.0/MultimachineWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultimachineWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532cf42b5a7d178c84493d925f7ca2c3b2e43812")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c408313dcb82d2f55afe6d74dbc7a0d64e8d80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultimachineWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultimachineWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
